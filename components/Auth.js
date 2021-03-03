314eacc910fbd4446d6defcbd4c2140aac5fb43c
Ryuyxx <ryuyxx@gmail.com>
2021-03-03 16:58:10 +0900

    add: fetch token

--- components/Auth.js
+++ components/Auth.js
@@ -1,6 +1,46 @@
-import Head from "next/head";
+import { useState } from "react";
+import { userRouter } from "next/router";
+import Cookie from "universal-cookie";
+
+const cookie = new Cookie();
 
 export default function Auth() {
+  const router = userRouter();
+  const [username, setUsername] = userState("");
+  const [password, setPassword] = userState("");
+  const [isLogin, setIsLogin] = userState(true);
+
+  const login = async () => {
+    try {
+      await fetch(
+        `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
+        {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          } else if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          const options = { path: "/" };
+          cookie.set("access_token", data.access, options);
+        });
+      router.push("/main-page");
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+
+    
   return (
     <div className="max-w-md w-full space-y-8">
       <div>
