17ba706a66fc75e62736d0ac022094b1fab8af0a
Ryuyxx <ryuyxx@gmail.com>
2021-03-03 17:11:08 +0900

    feat: auth user
    動的画面変更

--- components/Auth.js
+++ components/Auth.js
@@ -39,8 +39,30 @@ export default function Auth() {
     }
   };
 
+  const authUser = async (c) => {
+    e.preventDefault();
+    if (isLogin) {
+      login();
+    } else {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          }
+        });
+        login();
+      } catch (err) {
+        alert(err);
+      }
+    }
+  };
 
-    
   return (
     <div className="max-w-md w-full space-y-8">
       <div>
@@ -50,32 +72,38 @@ export default function Auth() {
           alt="Workflow"
         />
         <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
-          Sign in to your account
+          {isLogin ? "Login" : "Sign up"}
         </h2>
       </div>
-      <form className="mt-8 space-y-6" action="#" method="POST">
+      <form className="mt-8 space-y-6" onSubmit={authUser}>
         <input type="hidden" name="remember" value="true" />
         <div className="rounded-md shadow-sm -space-y-px">
           <div>
             <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
+              name="username"
+              type="text"
+              autoComplete="username"
               required
               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Email address"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
             />
           </div>
           <div>
             <input
-              id="password"
               name="password"
               type="password"
               autoComplete="current-password"
               required
               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
           </div>
         </div>
@@ -83,7 +111,7 @@ export default function Auth() {
         <div className="flex items-center justify-center">
           <div className="text-sm">
             <span
-              href="#"
+              onClick={() => setIsLogin(!isLogin)}
               className="font-medium text-white hover:text-indigo-500 cursor-pointer"
             >
               Register?
@@ -111,7 +139,7 @@ export default function Auth() {
                 />
               </svg>
             </span>
-            Sign in
+            {isLogin ? "Login" : "Register"}
           </button>
         </div>
       </form>
