a074fd71d758d4b5fbad6338c4ad7a65c36c308b
Ryuyxx <ryuyxx@gmail.com>
2021-03-04 16:46:32 +0900

    add: buttons on main
    task & blog page button

--- pages/main-page.js
+++ pages/main-page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import Cookie from "universal-cookie";
 import Layout from "../components/Layout";
@@ -12,6 +13,18 @@ export default function MainPage() {
   };
   return (
     <Layout title="Main Page">
+      <div className="mb-10">
+        <Link href="/blog-page">
+          <a className=" bg-indigo-500 mr-8  hover:bg-indigo-600 text-white px-4 py-12 rounded ">
+            Visit My Blog (SSG + ISR)
+          </a>
+        </Link>
+        <Link href="/task-page">
+          <a className="bg-gray-500 ml-8 hover:bg-gray-600 text-white px-4 py-12 rounded">
+            Visit Task (ISR + CSR)
+          </a>
+        </Link>
+      </div>
       <svg
         onClick={logout}
         className="mt-10 cursor-pointer w-6 h-6"
